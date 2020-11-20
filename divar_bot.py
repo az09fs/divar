a61212bc93417b8798cbf619a84a55abb44fd46f
az09fs <az09fs@gmail.com>
2020-11-20 07:36:00 +0330

    add page number and message

--- divar_bot.py
+++ divar_bot.py
@@ -8,6 +8,9 @@ import requests
 search = input('what you want?\n')
 city = input(f'which city? (type in english):\n')
 phone_number = input('please enter your phone number: +98')
+message = input('what you want to ask?\n')
+page = input('how many divar pages you want to ask the price?\n')
+page = int(page)+1
 driver = webdriver.Chrome()
 driver.get('https://www.divar.ir/')
 driver.find_element_by_xpath('//*[@id="app"]/div[1]/nav/div[2]/div[2]/i').click()
@@ -25,7 +28,7 @@ driver.find_element_by_xpath('/html/body/div[6]/div/div/div[2]/div/div[2]/div[1]
 l1=[]
 l2=[]
 l3=[]
-for i in range (1,3):
+for i in range (1,page):
     i = str(i)
     r = requests.get(f'https://divar.ir/s/'+city+'?q='+search+'&page='+i)
     link = re.findall(r'(/v/.*?)\"',r.text)
@@ -37,6 +40,7 @@ for k in range(0,len(l1)):
     driver.get(f'{l1[k]}')
     try:
         driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div[1]/div/div[2]/button[2]').click()
+        assert driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div[1]/div/div[4]/div[2]/div[2]/p').text == "توافقی"
         try:
             driver.find_element_by_xpath('/html/body/div[6]/div/article/footer/button').click()
         except:
@@ -57,7 +61,8 @@ for m in range(0,len(l3)):
     driver.get(f'{l3[m]}')
     time.sleep(10)
     try:
-        driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div[3]/div/div[3]/div[1]/button[1]').click()
+        driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div[3]/div/div[3]/div[2]/div/textarea').send_keys(message)
+        driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div[3]/div/div[3]/div[2]/div/img').click()
         time.sleep(2)
     except:
         pass
